5ff700ac241357aa04b09e0562c6797bb359812b
Remco Swenker <rswenker@gmail.com>
2020-02-15 16:26:17 +0100

    Simple member stuff to start with

--- members/models.py
+++ members/models.py
@@ -5,3 +5,11 @@ from django.db import models
 
 class Member(models.Model):
     name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    addressLineOne = models.CharField(max_length=255)
+    addressLineTwo = models.CharField(max_length=255)
+    addressLineThree = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=32)
+    studentNumber = models.CharField(max_length=32)
+    notes = models.CharField(max_length=1023)
