ee2a009a8a7d7eb24ea952b813705b812de3bd78
Remco Swenker <rswenker@gmail.com>
2020-02-20 21:02:14 +0100

    member view added

--- members/views.py
+++ members/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'members_base.html', {})
+    return render(request, 'members_base.html', {'members': Member.objects.all()})
 
+
+def show(request, member_id):
+    return render(request, 'member_show.html', {'member': Member.objects.get(pk=member_id)})
