cd48b516b640342229cddc4960bd5b3ad6cba410
Nander Voortman <nander@bellettrie.utwente.nl>
2020-02-27 17:05:24 +0100

    Switched to differently derpy sidebar; added link for holiday setup.

--- lendings/urls.py
+++ lendings/urls.py
@@ -4,7 +4,7 @@ from members.views import MemberList
 from . import views
 
 urlpatterns = [
-    path('', MemberList.as_view(), name='index'),
+    path('', MemberList.as_view(), name='members-list'),
     path('work/<int:work_id>', views.work_based, name='lend_from_work'),
     path('finalize/<int:work_id>/<int:member_id>', views.finalize, name='finalize_lending'),
 ]
