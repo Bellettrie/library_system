0770a129b133fe779d8218b0ebf3e4a9568b21f3
Nander Voortman <nanderv@gmail.com>
2020-03-28 15:34:14 +0100

    Fix typo

--- lendings/models.py
+++ lendings/models.py
@@ -28,5 +28,5 @@ class Reservation(models.Model):
     member = models.ForeignKey(Member, on_delete=PROTECT)
     item = models.ForeignKey("works.Item", on_delete=PROTECT)
     reserved_on = models.DateField()
-    reserved_by = models.ForeignKey(Member, on_delete=PROTECT, related_name="reservatin_action_by")
+    reserved_by = models.ForeignKey(Member, on_delete=PROTECT, related_name="reservation_action_by")
     reservation_end_date = models.DateField(null=True, blank=True)
