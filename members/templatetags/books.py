58e02758ebb94e8ff7409a5137a456b1a28aec84
Nander Voortman <nanderv@gmail.com>
2020-03-28 15:32:29 +0100

    Add handed in to list.

--- members/templatetags/books.py
+++ members/templatetags/books.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.inclusion_tag('publication_table/publication_table.html')
 def get_user_books(member: Member, perms):
     result = []
-    for lending in member.lending_set.all():
+    for lending in member.lending_set.filter(handed_in=False):
         it = ItemRow(lending.item, extra_info="Return date: " + str(lending.end_date))
         result.append(BookResult(lending.item.publication, [it], item_options=["extend", "return"]))
     return {"member": member, "perms": perms, "contents": result}
