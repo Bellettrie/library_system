9998cc3959661856a1efead36e31c407c2c0079b
Nander Voortman <nanderv@gmail.com>
2020-04-10 13:34:11 +0200

    Changed function to calculate fines, add first tests for it.

--- config/tests.py
+++ config/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from config.models import Holiday, LendingSettings
-from members.models import Member
+from members.models import Member, Committee
 from works.models import Publication, Item, Category, Location, ItemType
 from works.tests import create_work
 
@@ -27,9 +27,13 @@ class LendingTermTestCase(TestCase):
         self.item = Item.objects.create(publication=p, old_id=0, hidden=False, location=location)
         self.member = Member.objects.create(end_date=datetime.date(2023, 4, 4))
         self.member2 = Member.objects.create(end_date=datetime.date(2021, 4, 4))
+        committee = Committee.objects.create(active_member_committee=True, name="Active com")
+        self.member3 = Member.objects.create(end_date=datetime.date(2021, 4, 4))
+        self.member3.committees.add(committee)
+        self.member3.save()
         LendingSettings.objects.create(item_type=item_type,
                                        term_for_inactive=7,
-                                       term_for_active=7,
+                                       term_for_active=14,
                                        hand_in_days=1,
                                        borrow_money_inactive=0,
                                        borrow_money_active=0,
@@ -50,3 +54,35 @@ class LendingTermTestCase(TestCase):
 
     def test_end_date_not_in_noliday(self):
         self.assertEqual(datetime.date(2022, 5, 8), LendingSettings.get_end_date(self.item, self.member, datetime.date(2022, 5, 1)))
+
+    def test_active_member(self):
+        self.assertEqual(datetime.date(2019, 1, 2), LendingSettings.get_end_date(self.item, self.member3, datetime.date(2018, 12, 31 - 6 - 7)))
+
+
+class FineTestCase(TestCase):
+    def setUp(self):
+        Holiday.objects.create(name='Sleeping holiday',
+                               starting_date=datetime.date(2022, 4, 4),
+                               ending_date=datetime.date(2022, 5, 4),
+                               skipped_for_fine=True)
+        Holiday.objects.create(name='Very Short Holiday',
+                               starting_date=datetime.date(2019, 1, 1),
+                               ending_date=datetime.date(2019, 1, 1),
+                               skipped_for_fine=True)
+
+        Holiday.objects.create(name='Very Short Holiday',
+                               starting_date=datetime.date(2018, 1, 2),
+                               ending_date=datetime.date(2018, 1, 2),
+                               skipped_for_fine=False)
+
+    def test_simple_fine(self):
+        self.assertEquals(11, LendingSettings.get_fine_days(datetime.date(2020, 1, 1), datetime.date(2020, 1, 12)))
+
+    def test_early(self):
+        self.assertEquals(0, LendingSettings.get_fine_days(datetime.date(2021, 1, 1), datetime.date(2020, 1, 12)))
+
+    def test_on_time(self):
+        self.assertEquals(0, LendingSettings.get_fine_days(datetime.date(2020, 1, 1), datetime.date(2020, 1, 1)))
+
+    def test_holiday_skip(self):
+        self.assertEquals(0, LendingSettings.get_fine_days(datetime.date(2018, 1, 1), datetime.date(2018, 1, 2)))
