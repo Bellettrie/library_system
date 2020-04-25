104aa08d886516c8b5751b944e02b2128b9510e5
Nander Voortman <nanderv@gmail.com>
2020-04-25 09:49:02 +0200

    Add item state, add automatic state switching when lending and returning.

--- works/forms.py
+++ works/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from django.forms import ModelForm
+
+from works.models import ItemState
 
 
 class SimpleWorkSearch(forms.Form):
     name = forms.CharField(required=False)
+
+
+class ItemStateCreateForm(ModelForm):
+    class Meta:
+        model = ItemState
+        fields = ['type', 'reason']
--- works/urls.py
+++ works/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from works.views import WorkList
+from works.views import WorkList, create_item_state
 from . import views
 
 urlpatterns = [
     path('search', WorkList.as_view(), name='works.list'),
+    path('item_state/<slug:item_id>', create_item_state, name='works.item_state.create'),
     path('<slug:pk>', views.WorkDetail.as_view(), name='work.view'),
 ]
