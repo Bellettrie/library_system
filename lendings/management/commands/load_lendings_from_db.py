45fe303e0f997c7bcdb35d53ae1cb020b9266f3b
Nander <onserver@nander.net>
2020-07-03 11:57:45 +0200

    Changes based on server needs

--- lendings/management/commands/load_lendings_from_db.py
+++ lendings/management/commands/load_lendings_from_db.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 from django.core.management.base import BaseCommand
 
-from bellettrie_library_system.settings import OLD_DB
+from bellettrie_library_system.settings import OLD_DB, OLD_USN, OLD_PWD
 from lendings.models import Lending
 from members.management.commands.namegen import generate_name, generate_full_name
 from members.models import Member
--- migrate_all.sh
+++ migrate_all.sh
@@ -1,12 +1,12 @@
 rm db.sqlite3
-python manage.py migrate
-python manage.py load_authors_from_db
-python manage.py load_works_from_db
+python3 manage.py migrate
+python3 manage.py load_authors_from_db
+python3 manage.py load_works_from_db
 
-python manage.py load_authors_matched_to_works
-python manage.py load_series_titles
-python manage.py load_members_from_db
-python manage.py pseudonymise
-python manage.py load_lendings_from_db
-python manage.py load_categories
-python manage.py add_committees
+python3 manage.py load_authors_matched_to_works
+python3 manage.py load_series_titles
+python3 manage.py load_members_from_db
+python3 manage.py pseudonymise
+python3 manage.py load_lendings_from_db
+python3 manage.py load_categories
+python3 manage.py add_committees
