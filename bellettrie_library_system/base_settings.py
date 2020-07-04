c0f71bc701d6ede4ead2200b05bfe71e13c856f1
Nander Voortman <nanderv@gmail.com>
2020-07-04 17:00:09 +0200

    Fix problems with formatting.

--- bellettrie_library_system/base_settings.py
+++ bellettrie_library_system/base_settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '9_meq=rl3q!wh4=lr4g9t)ra9l*o_d7!exbh&^brhj=*_xm5y*'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "nander.net", "127.0.0.1"]
-
 
 # Application definition
 
@@ -44,7 +42,7 @@ INSTALLED_APPS = [
     'lendings',
     'news',
     'config',
-    'inventarisation'
+    'inventarisation',
 ]
 
 MIDDLEWARE = [
@@ -62,36 +60,23 @@ ROOT_URLCONF = 'bellettrie_library_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'builtins':
                 [
                     'bellettrie_library_system.templatetags.paginator_tag'
                 ],
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+            'context_processors':
+                [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
         },
     },
 ]
-
-WSGI_APPLICATION = 'bellettrie_library_system.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -112,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -126,24 +110,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/root/'
-#--------------------------------------------------
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-#-----------------------------------------------------
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
-os.path.join(BASE_DIR, 'bootstrap'),
-]
-
-OLD_DB = "oldsystem2"
-OLD_USN = "root"
-OLD_PWD = "root"
-
 DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap.html"
 
-LOGOUT_REDIRECT_URL='/accounts/login'
-LOGIN_REDIRECT_URL='/lend/me'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/lend/me'
---
+++ bellettrie_library_system/settings.py
@@ -0,0 +1,28 @@
+from bellettrie_library_system.base_settings import *
+import os
+WSGI_APPLICATION = 'bellettrie_library_system.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+STATIC_URL = '/root/'
+# --------------------------------------------------
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+# -----------------------------------------------------
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bootstrap'),
+]
+
+OLD_DB = "oldsystem2"
+OLD_USN = 'root'
+OLD_PWD = 'root'
+
