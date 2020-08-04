4e1bb01241d058a4c1b65d3018e0c83559add35a
Nander Voortman <nanderv@gmail.com>
2020-08-04 20:26:37 +0200

    Recoding storage done. Todo: actual logic for starting a recoding.

--- recode/models.py
+++ recode/models.py
@@ -7,6 +7,7 @@ from book_code_generation.models import BookCode
 from works.models import Item
 
 
-class Recode(models.Model, BookCode):
+class Recode(BookCode):
     item = models.ForeignKey(Item, on_delete=CASCADE)
+    book_code_extension = models.CharField(max_length=16)  # Where in the library is it?
 
