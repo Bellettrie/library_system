5b242844fec648e1f37f568e3b076413c0f6fd21
Nander Voortman <nanderv@gmail.com>
2020-08-04 20:38:02 +0200

    Lint fixes.

--- recode/models.py
+++ recode/models.py
@@ -10,4 +10,3 @@ from works.models import Item
 class Recode(BookCode):
     item = models.ForeignKey(Item, on_delete=CASCADE)
     book_code_extension = models.CharField(max_length=16)  # Where in the library is it?
-
