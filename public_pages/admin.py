a7177fe6235109bdbbfa6f88509c5e575fa8b103
Nander Voortman <nanderv@gmail.com>
2020-08-07 14:11:21 +0200

    Update admin py

--- public_pages/admin.py
+++ public_pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from public_pages.models import PublicPageGroup, PublicPage
+
+admin.register(PublicPageGroup)
+admin.register(PublicPage)
