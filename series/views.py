9bf79615022b119295dfb4a2731da17dc2ba3bad
Nander Voortman <nander@bellettrie.utwente.nl>
2020-09-09 18:39:50 +0200

    Fixes linting.

--- series/views.py
+++ series/views.py
@@ -91,12 +91,9 @@ def delete_series(request, pk):
     series = Series.objects.filter(pk=pk)
     z = SeriesNode.objects.filter(part_of_series=series.first())
     if len(z) > 0:
-        return render(request, 'are-you-sure.html',
-                      {'what': "To delete " + (
-                                  series.first().title or "<No name> ") + ", it has to have no sub-series."})
+        return render(request, 'are-you-sure.html', {'what': "To delete " + (series.first().title or "<No name> ") + ", it has to have no sub-series."})
     if not request.GET.get('confirm'):
-        return render(request, 'are-you-sure.html',
-                      {'what': "delete series with name " + (series.first().title or "<No name> ")})
+        return render(request, 'are-you-sure.html', {'what': "delete series with name " + (series.first().title or "<No name> ")})
     series.delete()
 
     return redirect('homepage')
