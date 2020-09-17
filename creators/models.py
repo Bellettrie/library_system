b3ebc86d606956b3447a755fbbc999c8c85eeb08
Nander Voortman <nander@bellettrie.utwente.nl>
2020-09-17 20:14:48 +0200

    Add creator relabeling feature

--- creators/models.py
+++ creators/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import PROTECT, CASCADE
 
 
+
+
 class Creator(models.Model):
     given_names = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255)
@@ -32,6 +34,13 @@ class Creator(models.Model):
                 result.append(item)
         return result
 
+    def get_all_series(self):
+        result = []
+        from series.models import Series
+        for work in Series.objects.filter(creatortoseries__creator=self):
+            result.append(work)
+        return result
+
 
 class CreatorRole(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -47,21 +56,67 @@ class CreatorLocationNumber(models.Model):
     letter = models.CharField(max_length=16)
 
 
-def force_relabel(creatorlocationnumber: CreatorLocationNumber, old_number: int, old_letter: str):
+def try_to_update_object(my_object, pattern, new_prefix):
+    match = pattern.match(my_object.book_code)
+    if match:
+        my_object.book_code = new_prefix + my_object.book_code[match.end():]
+        print(my_object.book_code)
+        my_object.save()
+def update_item(item, creatortonumber, pattern, old_prefix, new_prefix):
     from recode.models import Recode
 
+    recodes = Recode.objects.filter(item=item)
+    if len(recodes) > 0:
+        recode = recodes[0]
+        try_to_update_object(recode, pattern, new_prefix)
+    else:
+        match = pattern.match(item.book_code)
+        if match:
+            new_code = new_prefix + item.book_code[match.end():]
+            Recode.objects.create(item=item, book_code=new_code, book_code_extension=item.book_code_extension)
+
+
+def force_relabel(creatorlocationnumber: CreatorLocationNumber, old_number: int, old_letter: str):
+    from series.models import WorkInSeries
+    from works.models import Publication, Item
     items = creatorlocationnumber.creator.get_all_items()
     location_code = creatorlocationnumber.location.category.code
     for item in items:
-        if item.location != creatorlocationnumber.location:
+        if item.location != creatorlocationnumber.location :
+            print("DEAD")
+
+        if item.publication.get_authors()[
+            0].creator != creatorlocationnumber.creator:
             items.remove(item)
+            print("undead")
+    import re
+
+    old_prefix = location_code + "-" + old_letter + "-" + str(old_number)
+    new_prefix = location_code + "-" + creatorlocationnumber.letter + "-" + str(creatorlocationnumber.number) + "-"
+    pattern = re.compile("^" + location_code + "-" + old_letter + "-" + str(old_number) + "0*-")
 
     for item in items:
-        recodes = Recode.objects.filter(item=item)
-        if len(recodes) > 0:
-            recode = recodes[0]
-            if recode.book_code_sortable.startswith(location_code + "-" + old_letter + "-" + str(old_number)):
-                print("Change recode")
-        else:
-            if item.book_code_sortable.startswith(location_code + "-" + old_letter + "-" + str(old_number)):
-                print("Change Item")
+        update_item(item, creatorlocationnumber, pattern, old_prefix, new_prefix)
+
+    all_series = creatorlocationnumber.creator.get_all_series()
+
+    series_handle_list = []
+    for series in all_series:
+        if series.location == creatorlocationnumber.location:
+            if pattern.match(series.book_code_sortable):
+                try_to_update_object(series, pattern, new_prefix)
+                series_handle_list.append(series)
+
+    to_handle = True
+    while to_handle:
+        to_handle = False
+        new_series_list = []
+        for series in series_handle_list:
+            to_handle = True
+            for work_in_series in WorkInSeries.objects.filter(part_of_series=series, is_primary=True):
+                items = Item.objects.filter(publication__workinseries=work_in_series)
+                for item in items:
+                    update_item(item, creatorlocationnumber, pattern, old_prefix, new_prefix)
+            if series.part_of_series:
+                new_series_list.append(series.part_of_series)
+        series_handle_list = new_series_list
---
+++ series/management/commands/extract_series_locations.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import Group
+
+from django.core.management.base import BaseCommand
+
+from book_code_generation.models import get_number_for_code
+from creators.models import Creator, CreatorLocationNumber
+from members.models import Committee
+from members.permissions import KASCO, BOARD, ADMIN, COMCO, BOOKBUYERS, KICKIN, LENDERS, BOOKS, WEB, KONNICHIWA, \
+    RETRIEVAL
+from series.models import WorkInSeries, Series
+from works.models import Item, Location, Publication
+
+
+def get_publication_location(publication: Publication):
+    item = Item.objects.filter(publication_id=publication.pk).first()
+    if item:
+        return item.location
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        series_dict = {}
+        series_killed = set()
+        act_set = set()
+        for work_in_series in WorkInSeries.objects.all():
+            data = series_dict.get(work_in_series.part_of_series)
+            if data is None:
+                data = get_publication_location(work_in_series.work)
+            if data != get_publication_location(work_in_series.work):
+                series_killed.add(work_in_series.part_of_series)
+            series_dict[work_in_series.part_of_series] = data
+            act_set.add(work_in_series.part_of_series)
+        for series in series_dict.keys():
+            if not series in series_killed:
+                series.location = series_dict[series]
+                series.save()
+
+        blacklist = set()
+        edited_something = True
+        while edited_something:
+            edited_something = False
+            for series in Series.objects.all():
+                pos = series.part_of_series
+                if pos is not None and pos not in blacklist:
+                    if pos.location is None:
+                        edited_something = True
+                        pos.location = series.location
+                        pos.save()
+                    else:
+                        if pos.location != series.location:
+                            edited_something = True
+                            pos.location = None
+                            pos.save()
+                            blacklist.add(pos)
+                            print(pos.get_canonical_title())
