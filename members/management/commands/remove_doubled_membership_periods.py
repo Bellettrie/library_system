02600792fb2c0704a65e6599cc34747035d90ae9
Nander Voortman <nanderv@gmail.com>
2021-01-30 14:55:07 +0100

    Improved the monkeypatch script.

--- members/management/commands/remove_doubled_membership_periods.py
+++ members/management/commands/remove_doubled_membership_periods.py
@@ -13,23 +13,30 @@ def overlaps(startA, endA, startB, endB):
     endB = endB or datetime.date(datetime.fromisoformat('2100-01-01'))
 
     return (startA <= endB) and (endA >= startB)
-def mig():
 
+
+def mig():
     to_delete = set()
     for member in Member.objects.all():
         for msp in MembershipPeriod.objects.filter(member=member):
             for msp2 in MembershipPeriod.objects.filter(member=member):
                 if msp != msp2 and msp2 not in to_delete and overlaps(msp.start_date, msp.end_date, msp2.start_date, msp2.end_date):
-                    if msp.start_date < msp2.start_date:
-                        msp.end_date = msp2.start_date
-                    else:
-                        msp2.end_date = msp.start_date
-                    msp.save()
-                    msp2.save()
-                    if msp.end_date and msp.start_date >= msp.end_date:
-                        to_delete.add(msp)
-                    if msp2.end_date and msp2.start_date >= msp2.end_date:
+                    if msp.member_background == msp2.member_background and msp.membership_type == msp2.membership_type:
+                        msp.start_date = min(msp.start_date, msp2.start_date)
+                        msp.end_date = max(msp.end_date, msp2.end_date)
+                        msp.save()
                         to_delete.add(msp2)
+                    else:
+                        if msp.start_date < msp2.start_date:
+                            msp.end_date = msp2.start_date
+                        else:
+                            msp2.end_date = msp.start_date
+                        msp.save()
+                        msp2.save()
+                        if msp.end_date and msp.start_date >= msp.end_date:
+                            to_delete.add(msp)
+                        if msp2.end_date and msp2.start_date >= msp2.end_date:
+                            to_delete.add(msp2)
     for z in to_delete:
         z.delete()
 
