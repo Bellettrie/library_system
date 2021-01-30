1f904898053955df6dacc745550526c7acf55179
Nander Voortman <nanderv@gmail.com>
2021-01-30 14:32:09 +0100

    New migration script.

---
+++ members/management/commands/remove_doubled_membership_periods.py
@@ -0,0 +1,41 @@
+from datetime import datetime, date
+
+from django.contrib.auth.models import Group
+
+from django.core.management.base import BaseCommand
+from members.models import Committee, Member, MembershipPeriod, MemberBackground, MembershipType
+
+
+def overlaps(startA, endA, startB, endB):
+    startA = startA or datetime.date(datetime.fromisoformat('1900-01-01'))
+    startB = startB or datetime.date(datetime.fromisoformat('1900-01-01'))
+    endA = endA or datetime.date(datetime.fromisoformat('2100-01-01'))
+    endB = endB or datetime.date(datetime.fromisoformat('2100-01-01'))
+
+    return (startA <= endB) and (endA >= startB)
+def mig():
+
+    to_delete = set()
+    for member in Member.objects.all():
+        for msp in MembershipPeriod.objects.filter(member=member):
+            for msp2 in MembershipPeriod.objects.filter(member=member):
+                if msp != msp2 and msp2 not in to_delete and overlaps(msp.start_date, msp.end_date, msp2.start_date, msp2.end_date):
+                    if msp.start_date < msp2.start_date:
+                        msp.end_date = msp2.start_date
+                    else:
+                        msp2.end_date = msp.start_date
+                    msp.save()
+                    msp2.save()
+                    if msp.end_date and msp.start_date >= msp.end_date:
+                        to_delete.add(msp)
+                    if msp2.end_date and msp2.start_date >= msp2.end_date:
+                        to_delete.add(msp2)
+    for z in to_delete:
+        z.delete()
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        mig()
