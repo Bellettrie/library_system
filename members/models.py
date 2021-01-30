c1c56f5bebc6ecd66c37e20076fcb69b20a6eff1
Nander Voortman <nanderv@gmail.com>
2021-01-30 15:09:37 +0100

    Small fixes.

--- members/models.py
+++ members/models.py
@@ -5,6 +5,16 @@ from django.db.models import CASCADE, PROTECT, Q
 
 from members.management.commands.namegen import generate_full_name
 
+PAST = datetime.date(datetime.fromisoformat('1900-01-01'))
+FUTURE = datetime.date(datetime.fromisoformat('2100-01-01'))
+
+def overlaps(startA, endA, startB, endB):
+    startA = startA or datetime.date(datetime.fromisoformat('1900-01-01'))
+    startB = startB or datetime.date(datetime.fromisoformat('1900-01-01'))
+    endA = endA or datetime.date(datetime.fromisoformat('2100-01-01'))
+    endB = endB or datetime.date(datetime.fromisoformat('2100-01-01'))
+
+    return (startA <= endB) and (endA >= startB)
 
 class Committee(models.Model):
     name = models.CharField(max_length=255)
@@ -149,6 +159,36 @@ class Member(MemberData):
         MemberLog.from_member(self)
         super().save(*args, **kwargs)
 
+
+    def try_and_delete_double_periods(self):
+        to_delete = set()
+
+        for msp in MembershipPeriod.objects.filter(member=self):
+            for msp2 in MembershipPeriod.objects.filter(member=self):
+                if msp != msp2 and msp2 not in to_delete and msp not in to_delete and overlaps(msp.start_date, msp.end_date, msp2.start_date, msp2.end_date):
+                    if msp.member_background == msp2.member_background and msp.membership_type == msp2.membership_type:
+                        msp.start_date = min(msp.start_date or FUTURE, msp2.start_date or FUTURE)
+
+                        msp.end_date = max(msp.end_date or FUTURE, msp2.end_date or FUTURE)
+                        if msp.start_date == FUTURE:
+                            msp.start_date = None
+                        if msp.end_date == PAST:
+                            msp.end_date = None
+                        msp.save()
+                        to_delete.add(msp2)
+                    else:
+                        if msp.start_date < msp2.start_date:
+                            msp.end_date = msp2.start_date
+                        else:
+                            msp2.end_date = msp.start_date
+                        msp.save()
+                        msp2.save()
+                        if msp.end_date and msp.start_date >= msp.end_date:
+                            to_delete.add(msp)
+                        if msp2.end_date and msp2.start_date >= msp2.end_date:
+                            to_delete.add(msp2)
+        for z in to_delete:
+            z.delete()
     def __str__(self):
         return self.name
 
