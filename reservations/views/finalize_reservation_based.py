03332e99123126ad0be8ab32a198f56f877166f9
Nander Voortman <nanderv@gmail.com>
2022-03-07 13:39:49 +0100

    Cleaned up reservation finalize file

--- reservations/views/finalize_reservation_based.py
+++ reservations/views/finalize_reservation_based.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import permission_required
 from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect, render
 
+from lendings.lendingException import LendingImpossibleException
 from lendings.procedures.get_end_date import get_end_date
-from lendings.procedures.new_lending import create_lending
+from lendings.procedures.new_lending import create_lending, new_lending
 from reservations.models import Reservation
 
 
@@ -15,18 +16,14 @@ def finalize_reservation_based(request, id):
     reservation = get_object_or_404(Reservation, pk=id)
     member = reservation.member
     item = reservation.item
-    if item.is_available_for_lending():
-        if request.method == 'POST':
-            if not member.can_lend_item(item):
-                return redirect('/lend/failed_lending/{}/{}/0'.format(item.id, member.id))
-            if not member.is_currently_member():
-                return redirect('/lend/failed_lending/{}/{}/1'.format(item.id, member.id))
-            if member.has_late_items():
-                return redirect('/lend/failed_lending/{}/{}/3'.format(item.id, member.id))
-            reservation.delete()
-            lending = create_lending(item, member, request.user.member, datetime.now().date())
+    if request.method == "POST":
+        try:
+            lending = new_lending(item, member, request.user.member, datetime.date(datetime.now()))
             return render(request, 'lending_finalized.html',
                           {'member': member, 'item': item, "date": lending.end_date})
-        return render(request, 'lending_finalize.html',
-                      {'member': member, 'item': item, "date": get_end_date(item, member, datetime.now().date())})
-    return redirect('/lend/failed_lending/{}/{}/2'.format(item.id, member.id))
+        except LendingImpossibleException as error:
+            return render(request, 'lending_cannot_lend.html',
+                          {'member': member, 'item': item, 'error': error})
+
+    return render(request, 'lending_finalize.html',
+                  {'member': member, 'item': item, "date": get_end_date(item, member, datetime.now().date())})
