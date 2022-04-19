1f0db8c56b57f375c7c66fde797a4b96e5f4f4a9
Remco Swenker <rswenker@gmail.com>
2022-04-19 13:46:01 +0200

    fix bug in returning books.

--- lendings/views/item_based.py
+++ lendings/views/item_based.py
@@ -7,7 +7,7 @@ from works.models import Item
 
 @permission_required('lendings.add_lending')
 def item_based(request, work_id):
-    from members.views import query_members
+    from members.views.member_list import query_members
     from utils.get_query_words import get_query_words
     words = get_query_words(request.GET.get("q"))
     members = query_members(words)
