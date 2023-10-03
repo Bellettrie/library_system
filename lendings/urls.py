e34fd7153b7b3e5a22fef58e1866ad0f667dd207
Remco Swenker <rswenker@gmail.com>
2023-10-03 15:14:02 +0200

    Added pagination to the lendings page.
    Used ListView for table generation.

--- lendings/urls.py
+++ lendings/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from lendings.path_names import LENDING_VIEW, LENDING_LIST, LENDING_NEW_WORK, LENDING_FINALIZE, \
     LENDING_MY_LENDINGS, LENDING_NEW_MEMBER, LENDING_RETURNBOOK, LENDING_EXTEND, LENDING_FAILED
 
-from .views.index import index
 from .views.extend import extend
 from .views.finalize import finalize
 from .views.item_based import item_based
 from .views.lending_failed import lending_failed
+from .views.lendings_list import LendingList
 from .views.me import me
 from .views.member_based import member_based
 from .views.register_returned import return_item
 
 urlpatterns = [
-    path('', index, name=LENDING_LIST),
+    path('', LendingList.as_view(), name=LENDING_LIST),
     path('work/<int:work_id>', item_based, name=LENDING_NEW_WORK),
     path('member/<int:member_id>', member_based, name=LENDING_NEW_MEMBER),
     path('finalize/<int:work_id>/<int:member_id>', finalize, name=LENDING_FINALIZE),
