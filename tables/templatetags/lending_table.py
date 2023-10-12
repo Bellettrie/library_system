b6e78ef0a822b66fabf1a5f0c85ea1cf5918d761
Nander Voortman <bellettrie@nander.net>
2023-10-12 18:16:24 +0200

    Lend me table.

--- tables/templatetags/lending_table.py
+++ tables/templatetags/lending_table.py
@@ -2,9 +2,9 @@ from typing import List
 
 from bellettrie_library_system.templatetags.paginator_tag import register
 from lendings.models import Lending
-from tables.buttons import LendingTableReturnButton
+from tables.buttons import LendingTableReturnButton, ExtendButton
 from tables.columns import TitleColumn, AllAuthorsColumn, BookCodeColumn, LentByColumn, FineColumn, ButtonsColumn, \
-    HandinDate
+    HandinDate, StartDate
 from tables.table import Table
 from tables.rows import LendingRow
 
@@ -12,10 +12,10 @@ cols = [
     BookCodeColumn(),
     TitleColumn(),
     AllAuthorsColumn(),
-    LentByColumn(),
+    (),
     HandinDate(),
     FineColumn(),
-    ButtonsColumn([LendingTableReturnButton()], "Return")
+    ButtonsColumn([ExtendButton()], "Controls")
 ]
 
 
@@ -25,3 +25,23 @@ def current_lendings(lendings: List[Lending], perms):
     for lending in lendings:
         rows.append(LendingRow(lending))
     return {'table': Table(rows, cols), 'perms': perms}
+
+
+
+cols_me = [
+    BookCodeColumn(),
+    TitleColumn(),
+    AllAuthorsColumn(),
+    StartDate(),
+    HandinDate(),
+    FineColumn(),
+    ButtonsColumn([ExtendButton()], "#")
+]
+
+
+@register.inclusion_tag('tables/items_table.html')
+def my_lendings(lendings: List[Lending], perms):
+    rows = []
+    for lending in lendings:
+        rows.append(LendingRow(lending))
+    return {'table': Table(rows, cols_me), 'perms': perms}
