589aaa6939ed6f1b578c957d7d708cfa7603efa2
Nander Voortman <bellettrie@nander.net>
2023-12-09 18:19:37 +0100

    Fix pseudonimisation logic.

--- members/management/commands/pseudonymise.py
+++ members/management/commands/pseudonymise.py
@@ -1,12 +1,13 @@
 from django.core.management.base import BaseCommand
 
-from members.models import Member
+from members.models import Member, MemberLog
 
 
 class Command(BaseCommand):
     help = 'Pseudonimises all membership data; removes log entries for old member data changes.'
 
     def handle(self, *args, **options):
+        MemberLog.objects.all().delete()
         members = Member.objects.all()
         for member in members:
             member.pseudonymise()
