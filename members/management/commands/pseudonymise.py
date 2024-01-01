1c2b8a277365b3970f8cef91ce413f5a9165e7f0
Nander Voortman <bellettrie@nander.net>
2024-01-01 13:19:39 +0100

    Fix comment; remove dead code.

--- members/management/commands/pseudonymise.py
+++ members/management/commands/pseudonymise.py
@@ -3,15 +3,8 @@ from django.core.management.base import BaseCommand
 from members.models import Member
 
 
-def get_name(x):
-    vn = x.get("voornaam")
-    if len(vn) == 0:
-        return x.get("naam")
-    return vn + " " + x.get("naam")
-
-
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Pseudonimises all membership data; removes log entries for old member data changes.'
 
     def handle(self, *args, **options):
         members = Member.objects.all()
