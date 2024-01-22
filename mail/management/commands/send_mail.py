e426476de45b9c834c0badc6700827b216e36934
Nander Voortman <nander@bellettrie.utwente.nl>
2024-01-22 20:31:40 +0100

    Attempt at mail fixing

--- mail/management/commands/send_mail.py
+++ mail/management/commands/send_mail.py
@@ -8,6 +8,8 @@ class Command(BaseCommand):
     help = 'send email'
 
     def handle(self, *args, **options):
+        print("HOI")
         for mail in MailLog.objects.filter(sent=False):
+            print("SEND")
             mail.send()
             return
