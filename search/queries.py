65c7a5080ed0381b9ff0ec295473460d0450eb7a
Nander Voortman <bellettrie@nander.net>
2024-02-28 20:45:27 +0100

    Remove unused imports.

--- search/queries.py
+++ search/queries.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 
 from search.models import get_words_in_str
-from works.models import Publication, ItemState, Category, Item
+from works.models import Publication, Category
 
 
 class SearchOp:
