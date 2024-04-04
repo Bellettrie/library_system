49a9efc2391f44e118c87bb863c86452225521b1
Remco Swenker <rswenker@gmail.com>
2024-04-04 09:32:34 +0200

    Removed pagination for reservations as they will never be high enough

--- lendings/views/me.py
+++ lendings/views/me.py
@@ -23,7 +23,7 @@ class MyLendingList(LoginRequiredMixin, ListView):
 def lendings_and_reservations(request):
     lendings = Lending.objects.filter(member_id=request.user.member.pk).order_by('-end_date')
     paginator = Paginator(lendings, 10)
-    page = request.GET.get('page1')
+    page = request.GET.get('page')
     try:
         lendings = paginator.page(page)
     except PageNotAnInteger:
@@ -32,14 +32,6 @@ def lendings_and_reservations(request):
         lendings = paginator.page(paginator.num_pages)
 
     reservations = Reservation.objects.filter(member_id=request.user.member.pk).order_by('-reserved_on')
-    paginator = Paginator(reservations, 10)
-    page = request.GET.get('page2')
-    try:
-        reservations = paginator.page(page)
-    except PageNotAnInteger:
-        reservations = paginator.page(1)
-    except EmptyPage:
-        reservations = paginator.page(paginator.num_pages)
 
     context = {'lendings': lendings, 'reservations': reservations}
     return render(request, 'lendings/detail.html', context)
