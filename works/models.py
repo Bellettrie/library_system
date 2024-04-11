b4b6e78a7e95253a8b073f961abfa7ee50b9ae83
Remco Swenker <rswenker@gmail.com>
2024-04-11 10:29:42 +0200

    don't need to check for end date at all

--- works/models.py
+++ works/models.py
@@ -256,19 +256,14 @@ class Item(NamedThing, BookCode):
         return self.in_available_state() and not self.is_lent_out()
 
     def is_reserved(self):
-        reservations = Reservation.objects.filter(Q(item=self)
-                                                  & (Q(reservation_end_date__isnull=True)
-                                                     | Q(reservation_end_date__gte=datetime.now())))
+        reservations = Reservation.objects.filter(item=self)
         return reservations.count() > 0
 
     def is_available_for_reservation(self):
         return self.in_available_state() and not self.is_reserved()
 
     def is_reserved_for(self, member):
-        reservations = Reservation.objects.filter(Q(item=self)
-                                                  & Q(member=member)
-                                                  & (Q(reservation_end_date__isnull=True)
-                                                     | Q(reservation_end_date__gte=datetime.now())))
+        reservations = Reservation.objects.filter(item=self, member=member)
         return reservations.count() > 0
 
     def current_lending(self):
