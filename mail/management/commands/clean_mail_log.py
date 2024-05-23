8e183c036b57a63ab7ee32dd83876306a50e5a05
Remco Swenker <rswenker@gmail.com>
2024-05-23 09:45:30 +0200

    missed import

--- mail/management/commands/clean_mail_log.py
+++ mail/management/commands/clean_mail_log.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from lendings.procedures.mail_late import late_mails
 from mail.models import MailLog
+from utils.time import get_today
 
 
 class Command(BaseCommand):
