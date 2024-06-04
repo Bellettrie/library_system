40bc8042eeb2a1b91b8110aaaed12bd84406f1bb
Remco Swenker <rswenker@gmail.com>
2024-06-04 10:21:02 +0200

    Made anonymisation test and fixed some anonymise checks so they work correctly

--- members/procedures/anonymise.py
+++ members/procedures/anonymise.py
@@ -56,13 +56,13 @@ def anonymise_except(member: Member, current_date: datetime.date) -> None:
         raise AnonymisationException("Already anonymised")
     if member.is_blacklisted:
         raise AnonymisationException("Is blacklisted")
-    if member.is_currently_member():
+    if member.is_currently_member(current_date):
         raise AnonymisationException("Is currently a member")
     if member.is_active():
         raise AnonymisationException("Member is still active")
-    if member.user is not None and member.user.last_login is not None and (member.user.last_login.date() - current_date).days < -400:
+    if member.user is not None and member.user.last_login is not None and (current_date - member.user.last_login).days < 400:
         raise AnonymisationException("Logged in recently;  will be anonymised in " + str(
-            400 - (member.user.last_login.date() - current_date).days) + " days.")
+            400 - (current_date - member.user.last_login).days) + " days.")
     if (current_date - member.last_end_date()).days < 800:
         raise AnonymisationException("Was recently a member; can be anonymised in " + str(
             800 - (current_date - member.last_end_date()).days) + " days.")
