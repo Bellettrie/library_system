3bb9b72159e11ebe35713ab2cd9d6234f4b971f4
Nander Voortman <nander.voortman@nedap.com>
2024-06-27 17:36:49 +0200

    Add new member filter layout.

--- datamining/urls.py
+++ datamining/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from lendings.path_names import LENDING_VIEW, LENDING_LIST, LENDING_NEW_WORK, LENDING_FINALIZE, \
     LENDING_MY_LENDINGS, LENDING_NEW_MEMBER, LENDING_RETURNBOOK, LENDING_EXTEND, LENDING_FAILED
 
-from .views import show_members, show_membership_stats, show_lending_stats
+from .views import show_members_by_date, show_members_by_group, show_membership_stats, show_lending_stats
 
 urlpatterns = [
-    path('member_filtered', show_members, name='datamining.members'),
+    path('member_filtered/date_based', show_members_by_date, name='datamining.members.date_based'),
+    path('member_filtered/special', show_members_by_date, name='datamining.members.special'),
+    path('member_filtered/groups', show_members_by_group, name='datamining.members.groups'),
+    path('member_filtered', show_members_by_date, name='datamining.members'),
+
     path('member_stats', show_membership_stats, name='datamining.membership_stats'),
     path('lending_stats', show_lending_stats, name='datamining.lending_stats')
 ]
