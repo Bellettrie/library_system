29fdabb9e531c7811992cd9c6c2f23da6f534c80
Nander Voortman <bellettrie@nander.net>
2024-09-08 13:30:07 +0200

    Add modal based flow for extending books: - Add htmx tot static stuff; - add base modal template - add templates for content of modal for extending - update extend function to also support htmx version - add extra url for extending with htmx-flow.

--- lendings/views/extend.py
+++ lendings/views/extend.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -14,8 +15,18 @@ from utils.time import get_today
 from works.models import Item
 
 
+def hx_extend(request, work_id):
+    return extend(request, work_id, hx_enabled=True)
 @transaction.atomic
-def extend(request, work_id):
+def extend(request, work_id, hx_enabled=False):
+    cannot_extend_template='lendings/cannot_extend.html'
+    extend_finished_template='lendings/extend_finished.html'
+    extend_template='lendings/extend.html'
+    if hx_enabled:
+        cannot_extend_template = 'lendings/cannot_extend_hx.html'
+        extend_finished_template = 'lendings/extend_finished_hx.html'
+        extend_template = 'lendings/extend_hx.html'
+
     item = get_object_or_404(Item, pk=work_id)
     lending = item.current_lending()
 
@@ -23,13 +34,13 @@ def extend(request, work_id):
     if not request.user.has_perm('lendings.extend'):
         if not hasattr(request.user, 'member'):
             # The user who tries to extend is not linked to a member
-            return render(request, 'lendings/cannot_extend.html',
+            return render(request, cannot_extend_template,
                           {'member': lending.member, 'item': lending.item,
                            'error': "Please contact the web committee, something is spectacularly wrong"})
         member = request.user.member
         if not (member and member == request.user.member):
             # A user without the extend any permission tries to extend someone elses book
-            return render(request, 'lendings/cannot_extend.html',
+            return render(request, cannot_extend_template,
                           {'member': lending.member, 'item': lending.item,
                            'error': "You lack the permissions to extend an item that you did borrow for yourself."})
     late_days = get_today() - lending.end_date
@@ -38,16 +49,16 @@ def extend(request, work_id):
     if request.method == 'POST':
         try:
             new_extension(lending, get_today())
-            return render(request, 'lendings/extend_finished.html',
+            return render(request, extend_finished_template,
                           {'member': lending.member,
                            'item': item,
                            "date": lending.end_date
                            })
         except LendingImpossibleException as error:
-            return render(request, 'lendings/cannot_extend.html',
+            return render(request, cannot_extend_template,
                           {'member': lending.member, 'item': lending.item, 'error': error})
     else:
-        return render(request, 'lendings/extend.html',
+        return render(request, extend_template,
                       {'member': lending.member,
                        'item': item,
                        'end_date': lending.end_date,
