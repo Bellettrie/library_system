ae98dde70e3b4797f193e13d0f84e3e469271ff5
Nander Voortman <bellettrie@nander.net>
2024-09-10 20:51:02 +0200

    fix codestyle

--- members/views/user/delete.py
+++ members/views/user/delete.py
@@ -10,6 +10,8 @@ from members.permissions import MEMBERS_VIEW
 
 def delete_user_hx(request, member_id):
     return delete_user(request, member_id, True)
+
+
 @transaction.atomic
 @permission_required('auth.delete_user')
 def delete_user(request, member_id, hx_enabled=False):
@@ -19,13 +21,15 @@ def delete_user(request, member_id, hx_enabled=False):
     member.save()
     user.delete()
     if hx_enabled:
-        return HttpResponse(status=209, headers={"HX-Redirect":"true"})
+        return HttpResponse(status=209, headers={"HX-Redirect": "true"})
     return HttpResponseRedirect(reverse(MEMBERS_VIEW, args=(member.pk, 0,)))
 
 
 def delete_user_prompt_hx(request, member_id):
     member = get_object_or_404(Member, pk=member_id)
     return render(request, 'users/delete_hx.html', {'member': member, 'member_user': member.user})
+
+
 @transaction.atomic
 @permission_required('auth.delete_user')
 def delete_user_prompt(request, member_id):
