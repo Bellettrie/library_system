99225a468692d33459f10cab7223def23da5056b
Nander Voortman <bellettrie@nander.net>
2024-09-10 20:51:02 +0200

    fix codestyle

--- members/views/user/edit.py
+++ members/views/user/edit.py
@@ -7,15 +7,17 @@ from django.urls import reverse
 
 from members.models import Member
 
+
 def change_user_hx(request, member_id):
     return change_user(request, member_id, True)
 
+
 @transaction.atomic
 @permission_required('auth.change_user')
-def change_user(request, member_id,hx_enabled=False):
+def change_user(request, member_id, hx_enabled=False):
     templ = 'users/edit.html'
     if hx_enabled:
-        templ='users/edit_hx.html'
+        templ = 'users/edit_hx.html'
     member = get_object_or_404(Member, pk=member_id)
     if request.method == 'POST':
         form = PasswordChangeForm(user=member.user, data=request.POST)
