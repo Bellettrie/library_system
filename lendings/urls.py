7e86a71cbdd3414de29d47e2f8c4ff0de1cd164a
Remco Swenker <rswenker@gmail.com>
2024-09-12 16:29:14 +0200

    First version of book return in HTMX

--- lendings/urls.py
+++ lendings/urls.py
@@ -12,19 +12,19 @@ from .views.lending_history import LendingHistory
 from .views.lendings_list import LendingList
 from .views.me import lendings_and_reservations
 from .views.member_based import member_based
-from .views.register_returned import return_item
+from .views.register_returned import return_item, hx_return_item
 
 urlpatterns = [
     path('', LendingList.as_view(), name=LENDING_LIST),
     path('work/<int:work_id>', item_based, name=LENDING_NEW_WORK),
     path('member/<int:member_id>', member_based, name=LENDING_NEW_MEMBER),
     path('finalize/<int:work_id>/<int:member_id>', finalize, name=LENDING_FINALIZE),
-
     path('finalize/hx/<int:work_id>/<int:member_id>', finalize_hx, name=LENDING_FINALIZE + "_hx"),
     path('failed_lending/<int:work_id>/<int:member_id>/<int:reason_id>', lending_failed, name=LENDING_FAILED),
     path('extend/<int:work_id>', extend, name=LENDING_EXTEND),
     path('extend/hx/<int:work_id>', hx_extend, name=LENDING_EXTEND + "_hx"),
     path('return/<int:work_id>', return_item, name=LENDING_RETURNBOOK),
+    path('return/hx/<int:work_id>', hx_return_item, name=LENDING_RETURNBOOK + "_hx"),
     path('me/', lendings_and_reservations, name=LENDING_MY_LENDINGS),
     path('history/<int:work_id>', LendingHistory.as_view(), name=LENDING_HISTORY),
 ]
