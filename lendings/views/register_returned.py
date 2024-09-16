dc4faf4706d0081c1ac8ae243f3368308ffb75cb
Remco Swenker <rswenker@gmail.com>
2024-09-16 16:30:30 +0200

    more layout fix

--- lendings/views/register_returned.py
+++ lendings/views/register_returned.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.db import transaction
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 # Create your views here.
@@ -9,6 +9,7 @@ from reservations.models import Reservation
 from utils.time import get_today
 from works.models import Item
 from lendings.procedures.register_returned import register_returned_with_mail
+
 
 @permission_required('lendings.return')
 def hx_return_item(request, work_id):
@@ -29,13 +30,14 @@ def return_item(request, work_id, hx_enabled=False):
     if request.method == 'POST':
         register_returned_with_mail(lending, request.user.member)
         if hx_enabled:
-            return render(request, 'lendings/returned_book_hx.html', {'item': item,
-                                                                      'member': lending.member})
+            return render(request, 'lendings/returned_book_hx.html',
+                          {'item': item, 'member': lending.member})
         return HttpResponseRedirect(reverse('members.view', args=(lending.member.pk, 0,)))
-    return render(request, return_book_template, {'item': item, 'lending': lending,
-                                                         'late': lending.end_date < get_today(),
-                                                         'days_late': late_days.days,
-                                                         'fine': lending.calculate_fine(),
-                                                         'reserved': len(reservations) > 0,
-                                                         'reservation': reservations.first(),
-                                                         'today': get_today()})
+    return render(request, return_book_template, {'item': item,
+                                                  'lending': lending,
+                                                  'late': lending.end_date < get_today(),
+                                                  'days_late': late_days.days,
+                                                  'fine': lending.calculate_fine(),
+                                                  'reserved': len(reservations) > 0,
+                                                  'reservation': reservations.first(),
+                                                  'today': get_today()})
