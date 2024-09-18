476e381f7eb5a739d0bbc0f00ef7739ca826f98d
Nander ... <nander@bellettrie.utwente.nl>
2024-09-18 09:35:25 +0200

    Update by_committee.py

--- members/views/auth/by_committee.py
+++ members/views/auth/by_committee.py
@@ -6,4 +6,4 @@ def webcie(request):
     committee = Committee.objects.filter(name__exact="Web Committee").first()
     if hasattr(request.user, 'member') and committee in request.user.member.committees.all():
         return HttpResponse(status=200)
-    return HttpResponse(content='  <meta http-equiv="refresh" content="0;URL=\'/accounts/login/?next=/dev/\'">',status=403)
+    return HttpResponse(content='  <meta http-equiv="refresh" content="0;URL=\'/accounts/login/?next=/dev/\'">', status=403)
