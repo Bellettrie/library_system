e7137e83f6a25833f42a470718241c5f86aef532
Nander Voortman <nander@nander.net>
2024-09-23 00:20:06 +0200

    Refactor code so old mail list gets cleaned out by old cron job when we merge this one in.

--- mail/models.py
+++ mail/models.py
@@ -14,7 +14,24 @@ from tasks.models import Task
 from utils.time import get_now
 
 
-class MailLog:
+class MailLog(models.Model):
+    member = models.ForeignKey(Member, on_delete=PROTECT)
+    contents = models.TextField()
+    subject = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    sent = models.BooleanField(default=True)
+
+    @transaction.atomic
+    def send(self):
+        msg = EmailMessage(subject=self.subject,
+                           body=self.contents,
+                           from_email="info@bellettrie.utwente.nl",
+                           to=[self.member.get_email()])
+        msg._is_rendered = True
+        msg.send()
+
+
+class MailTask:
     def __init__(self, member, contents, subject, date):
         self.member = member
         self.contents = contents
@@ -39,6 +56,5 @@ def mail_member(template_string: str, context: dict, member: Member, now=None):
     if not member.is_anonymous_user:
         m = EmailMessage(template_string, context, "info@bellettrie.utwente.nl", render=True)
 
-        t = Task(task_name="send_mail",
-                 task_object=MailLog(member=member, contents=m.body, subject=m.subject, date=now))
+        t = Task(task_name="send_mail", task_object=MailTask(member=member, contents=m.body, subject=m.subject, date=now))
         t.save()
