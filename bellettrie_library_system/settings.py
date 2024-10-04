399cf701d4d0a782630afa9128d617dea41bc697
Nander Voortman <nander@nander.net>
2024-10-04 23:25:38 +0200

    add release version

--- bellettrie_library_system/settings.py
+++ bellettrie_library_system/settings.py
@@ -13,6 +13,7 @@ WSGI_APPLICATION = 'bellettrie_library_system.wsgi.application'
 ALLOWED_HOSTS = ["*"]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+VERSION = env("VERSION", default="UNKNOWN VERSION")
 LIBRARY_NAME = env("LIBRARY_NAME", default="Bellettrie")
 LIBRARY_IMAGE_URL = env("LIBRARY_IMAGE_URL", default="images/wurm.png")
 LIBRARY_DESCRIPTION = env("LIBRARY_DESCRIPTION",
