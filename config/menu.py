dcbec916208e62658bcd3d3c140292c8d7533b1c
Nander Voortman <nander@nander.net>
2024-10-06 18:06:54 +0200

    remove blank lines

--- config/menu.py
+++ config/menu.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
-
-
 class MenuItem:
-    def __init__(self, title, url, permission, location, sub_items, anonymous=None, only_subitems=False, icon=None, is_logout=False):
+    def __init__(self, title, url, permission, location, sub_items, anonymous=None, only_subitems=False, icon=None,
+                 is_logout=False):
         self.title = title
         self.url = url
         self.permission = permission
@@ -17,8 +16,9 @@ class MenuItem:
         self.is_logout = is_logout
 
     def permits(self, request):
-        return (self.permission is None or request.user.has_perm(self.permission)) and (self.anonymous is None or request.user.is_anonymous == self.anonymous) and (
-            not self.only_subitems or len(self.rendered_sub_items(request)) > 0)
+        return (self.permission is None or request.user.has_perm(self.permission)) and (
+                    self.anonymous is None or request.user.is_anonymous == self.anonymous) and (
+                not self.only_subitems or len(self.rendered_sub_items(request)) > 0)
 
     def rendered_sub_items(self, request):
         lst = []
@@ -30,7 +30,8 @@ class MenuItem:
 
     def render(self, request):
         sub_items = self.rendered_sub_items(request)
-        return render(request, 'config/render_menu_item.html', context={'menu': self, 'has_sub_items': len(sub_items) > 0, 'sub_items': sub_items})
+        return render(request, 'config/render_menu_item.html',
+                      context={'menu': self, 'has_sub_items': len(sub_items) > 0, 'sub_items': sub_items})
 
     def get_title_shortened(self):
         return self.title.replace(" ", "")
