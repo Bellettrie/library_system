358e01b356146098c70f247b87bffa144cd5ee4f
Nander Voortman <nander@nander.net>
2024-10-15 18:58:28 +0200

    Fix formatting.

--- works/components/table.py
+++ works/components/table.py
@@ -1,12 +1,13 @@
 # In a file called [project root]/components/calendar/calendar.py
 from django_components import Component, register
 
+
 @register("item_table")
 class ItemTable(Component):
     template_name = "table.html"
+
     # This component takes one parameter, a date string to show in the template
     def get_context_data(self, items):
         return {
             "items": items,
         }
-
