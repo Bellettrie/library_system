a1f5cfa73c7a95923c1294f430b8a9013047dc53
Nander Voortman <nander@nander.net>
2024-11-08 20:21:31 +0100

    Add basic sidebar

--- components/nav/navbar.py
+++ components/nav/navbar.py
@@ -1,9 +1,41 @@
 # In a file called [project root]/components/calendar/calendar.py
+from typing import Optional
+
+from django.template.context import Context
+
 from django.urls import reverse
 from django_components import Component, register, types
 
 from bellettrie_library_system import settings
 from bellettrie_library_system.base_settings import GET_MENU
+
+
+def get_top_menu():
+    pass
+
+
+@register("top-item")
+class NavItem(Component):
+
+    # This component takes one parameter, a date string to show in the template
+    def get_context_data(self, text, my_url, *args, location="aaa", perm=None, **kwargs):
+        # skip absolute urls
+        if not (my_url =="" or my_url.startswith("/") or my_url.startswith("https://")):
+            my_url = reverse(my_url, args=args)
+        return {
+            "my_url": my_url,
+            "text": text,
+            "location": location,
+            "perm": perm
+        }
+
+    def get_template_name(self, context: Context) -> Optional[str]:
+        l = context.get("location")
+        if l == "top":
+            return "nav/items/top_menu.html"
+        if l == "mob":
+            return "nav/items/mobile.html"
+        return "nav/items/sidebar.html"
 
 
 @register("top_nav")
@@ -13,13 +45,15 @@ class TopNav(Component):
     # This component takes one parameter, a date string to show in the template
     def get_context_data(self):
         left_items = []
-        right_items = []
+        mobile_items = []
         for it in GET_MENU():
-            if it.location == "top-left":
+            if it.location == "top":
                 left_items.append(it)
-            if it.location == "top-right":
-                right_items.append(it)
+                mobile_items.append(it)
+
         return {
+            "menu_buttons": left_items,
+            "mobile_items": mobile_items,
             "logo_debug": settings.UPSIDE_DOWN,
             "logo_name": settings.LIBRARY_NAME,
             "logo_image": settings.LIBRARY_IMAGE_URL,
@@ -51,25 +85,6 @@ class Logo(Component):
 }
 
 """
-
-
-@register("top-item")
-class TopMenuItem(Component):
-
-    # This component takes one parameter, a date string to show in the template
-    def get_context_data(self, text, my_url, *args):
-        # skip absolute urls
-        if not (my_url.startswith("/") or my_url.startswith("https://")):
-            my_url = reverse(my_url, args=args)
-
-        return {
-            "my_url": my_url,
-            "text": text,
-        }
-
-    template: types.django_html = """
-            <li><a href="{{ my_url }}" >{{ text }}</a></li>
-    """
 
 
 @register("top-item-logout")
