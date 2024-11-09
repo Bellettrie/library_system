689d63a6ce0b82c167e8151e12db877aad96c2ff
Nander Voortman <nander@nander.net>
2024-11-09 11:06:40 +0100

    Update tailwind stuff

---
+++ components/help_bar.py
@@ -0,0 +1,14 @@
+# In a file called [project root]/components/calendar/calendar.py
+from typing import Optional
+
+from django.template.context import Context
+
+from django.urls import reverse
+from django_components import Component, register, types
+
+from bellettrie_library_system import settings
+from bellettrie_library_system.base_settings import GET_MENU
+
+@register("help_bar")
+class HelpBar(Component):
+    template_name = "help_bar.html"
