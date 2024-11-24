4eb0a47d0ea2d56308612b46b4d5351ba46a4301
Nander Voortman <nander@nander.net>
2024-11-24 19:15:10 +0100

    rework member page to use standard widget component

--- components/widget/widget.py
+++ components/widget/widget.py
@@ -4,4 +4,9 @@ from django_components import Component, register, types
 
 @register("widget/widget_base")
 class BaseWidget(Component):
+    def get_context_data(self, root_class_tags=""):
+
+        return {
+            "root_class_tags":root_class_tags,
+        }
     template_name = "widget/widget.html"
