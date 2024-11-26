a5e84c64acb5edec75d4ed9cabc9b5d2663e1447
Nander Voortman <nander.voortman@nedap.com>
2024-11-26 09:33:34 +0100

    Rename widget to tiles.

--- components/tile/tile.py
+++ components/tile/tile.py
@@ -2,11 +2,11 @@ from django.urls import reverse
 from django_components import Component, register, types
 
 
-@register("widget/widget_base")
+@register("tile/tile")
 class BaseWidget(Component):
     def get_context_data(self, root_class_tags=""):
 
         return {
             "root_class_tags":root_class_tags,
         }
-    template_name = "widget/widget.html"
+    template_name = "tile/tile.html"
