c107c7649f415203494abc16610c2218dc06cbef
Remco Swenker <rswenker@gmail.com>
2024-12-03 12:56:20 +0100

    Was mistaken in the type hint

--- config/components/config/holiday.py
+++ config/components/config/holiday.py
@@ -1,10 +1,14 @@
+from typing import List
+
 from django_components import Component, register
+
+from config.models import Holiday
 
 
 @register("config.holiday_table")
 class HolidayTable(Component):
 
-    def get_context_data(self, holidays=None):
+    def get_context_data(self, holidays:List[Holiday]=None):
         return {
             "holidays": holidays,
         }
