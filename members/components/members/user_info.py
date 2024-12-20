96488eb48dd322a6e16664569419e0a6d5064e44
Nander Voortman <nander@nander.net>
2024-12-20 15:22:57 +0100

    Layouting updates for members page

---
+++ members/components/members/user_info.py
@@ -0,0 +1,19 @@
+from django_components import Component, register
+
+@register("members.users.no_user")
+class NoUser(Component):
+    def get_context_data(self, member=""):
+        return {
+            "member": member,
+        }
+
+    template_name = "members/users/no_user.html"
+
+@register("members.users.user")
+class NoUser(Component):
+    def get_context_data(self, member=""):
+        return {
+            "member": member,
+        }
+
+    template_name = "members/users/existing_user.html"
