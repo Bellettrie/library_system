9f686481b7d461dc31930a6bf3e195deb3ad77cb
Nander Voortman <nander@nander.net>
2024-12-29 19:27:58 +0100

    Fix some layouting issues with page editing.

--- bellettrie_library_system/base_settings.py
+++ bellettrie_library_system/base_settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_components',
+    'widget_tweaks',
     'mail_templated',
     'widget_tweaks',
     'works',
