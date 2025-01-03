d53517c4442b027eedfaccd21af80102f8d38711
Nander Voortman <nander@nander.net>
2025-01-03 09:27:40 +0100

    Add form component.

--- bellettrie_library_system/base_settings.py
+++ bellettrie_library_system/base_settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.forms',
     'django_components',
     'mail_templated',
     'widget_tweaks',
@@ -230,3 +231,5 @@ COMPONENTS = {
         os.path.join(BASE_DIR, "components"),
     ],
 }
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
