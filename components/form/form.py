4523322fa02da01d04c41771d86a490d406f56e9
Nander Voortman <nander@nander.net>
2025-01-03 10:02:18 +0100

    fix linting

--- components/form/form.py
+++ components/form/form.py
@@ -9,17 +9,17 @@ from django_components import Component, register, types
 class Form(Component):
     template_name = "form/form.html"
 
-    def get_context_data(self, form:Form):
+    def get_context_data(self, form: Form):
         return {
             "form": form,
         }
+
 
 @register("form/compact")
 class CompactForm(Component):
     template_name = "form/compact.html"
 
-    def get_context_data(self, form:Form):
-
+    def get_context_data(self, form: Form):
         return {
             "form": form,
         }
