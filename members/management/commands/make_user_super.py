f08af2ad028f3e85e7266661d13e05576033c895
Nander <nanderv@gmail.com>
2025-01-13 21:10:57 +0100

    Fix user super making

--- members/management/commands/make_user_super.py
+++ members/management/commands/make_user_super.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import Group, Permission, User
 from django.core.management.base import BaseCommand
 
 from members.models import Committee
-from members.permissions import KASCO, BOARD, ADMIN, COMCO, BOOKBUYERS, KICKIN, LENDERS, BOOKS, WEB, KONNICHIWA, RETRIEVAL
+from members.permissions import KASCO, BOARD, ADMIN, COMCO, BOOKBUYERS, KICKIN, LENDERS, BOOKS, WEB, KONNICHIWA, \
+    RETRIEVAL
 
 
 class Command(BaseCommand):
@@ -13,7 +14,7 @@ class Command(BaseCommand):
         parser.add_argument('user', type=str, help='User to make super')
 
     def handle(self, *args, **options):
-        user=User.objects.get(username=options['user'])
-        user.is_superuser=True
-        user.is_staff=True
+        user = User.objects.get(username=options['user'])
+        user.is_superuser = True
+        user.is_staff = True
         user.save()
