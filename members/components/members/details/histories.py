49cebf4aa8544aa548acf42085ab9e3cbf4b4ea9
Nander <nanderv@gmail.com>
2025-01-13 21:20:40 +0100

    Update lending list of member as well.

--- members/components/members/details/histories.py
+++ members/components/members/details/histories.py
@@ -2,14 +2,17 @@ from typing import List
 
 from django_components import Component, register
 
+from lendings.models import Lending
 from members.models import Member
 
 
 @register("members.current_lendings")
 class LendingHistory(Component):
     def get_context_data(self, member: Member):
+
         return {
-            "member": member
+            "member": member,
+            "lendings":Lending.objects.filter(member=member, handed_in=False)
         }
 
     template_name = "members/details/history_lending.html"
