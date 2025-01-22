b44d79fbe06e22322c71785aaf515f910e292e23
Nander Voortman <nanderv@gmail.com>
2025-01-22 21:20:05 +0100

    Remove dead code.

--- lendings/urls.py
+++ lendings/urls.py
@@ -1,31 +1,24 @@
 from django.urls import path
 
-from lendings.path_names import LENDING_VIEW, LENDING_LIST, LENDING_NEW_WORK, LENDING_FINALIZE, \
-    LENDING_MY_LENDINGS, LENDING_NEW_MEMBER, LENDING_RETURNBOOK, LENDING_EXTEND, LENDING_FAILED, LENDING_HISTORY
 from utils.wrappers import hx_wrap
 
-from .views.extend import extend, hx_extend
-from .views.finalize import finalize, finalize_hx
+from .views.extend import extend
+from .views.finalize import finalize
 
 from .views.item_based import item_based
-from .views.lending_failed import lending_failed
 from .views.lending_history import LendingHistory
 from .views.lendings_list import LendingList
 from .views.me import lendings_and_reservations
 from .views.member_based import member_based
-from .views.register_returned import return_item, hx_return_item
+from .views.register_returned import return_item
 
 urlpatterns = [
-    path('', LendingList.as_view(), name=LENDING_LIST),
-    path('work/<int:work_id>', item_based, name=LENDING_NEW_WORK),
-    path('member/<int:member_id>', member_based, name=LENDING_NEW_MEMBER),
-    path('finalize/<int:work_id>/<int:member_id>', finalize, name=LENDING_FINALIZE),
-    path('finalize/hx/<int:work_id>/<int:member_id>', finalize_hx, name=LENDING_FINALIZE + "_hx"),
-    path('failed_lending/<int:work_id>/<int:member_id>/<int:reason_id>', lending_failed, name=LENDING_FAILED),
-    path('extend/<int:work_id>', extend, name=LENDING_EXTEND),
-    path('extend/hx/<int:work_id>', hx_extend, name=LENDING_EXTEND + "_hx"),
-    path('return/<int:work_id>', return_item, name=LENDING_RETURNBOOK),
-    path('return/hx/<int:work_id>', hx_wrap(return_item), name=LENDING_RETURNBOOK + "_hx"),
-    path('me/', lendings_and_reservations, name=LENDING_MY_LENDINGS),
-    path('history/<int:work_id>', LendingHistory.as_view(), name=LENDING_HISTORY),
+    path('', LendingList.as_view(), name='lendings.list'),
+    path('new_from_work/<int:work_id>', item_based, name='lendings.new.work'),
+    path('new_from_member/<int:member_id>', member_based, name='lendings.new.member'),
+    path('finalize/<int:work_id>/<int:member_id>', hx_wrap(finalize), name='lendings.finalize'),
+    path('extend/<int:work_id>', hx_wrap(extend), name='lendings.extend'),
+    path('return/<int:work_id>', hx_wrap(return_item), name='lendings.returnbook'),
+    path('me/', lendings_and_reservations, name='lendings.me'),
+    path('history/<int:work_id>', LendingHistory.as_view(), name='lendings.history'),
 ]
--- lendings/views/register_returned.py
+++ lendings/views/register_returned.py
@@ -14,20 +14,19 @@ from lendings.procedures.register_returned import register_returned_with_mail
 @transaction.atomic
 @permission_required('lendings.return')
 def return_item(request, work_id, hx_enabled=False):
-    return_book_template = 'lendings/return_book.html'
-
-
+    return_book_template = 'lendings/modals/return_book.html'
     item = get_object_or_404(Item, pk=work_id)
     lending = item.current_lending()
+
     late_days = get_today() - lending.end_date
     reservations = Reservation.objects.filter(item=item)
     if request.method == 'POST':
         register_returned_with_mail(lending, request.user.member)
         if hx_enabled:
-            return render(request, 'lendings/returned_book.html',
+            return render(request, 'lendings/modals/returned_book.html',
                           {'item': item, 'member': lending.member})
         return HttpResponseRedirect(reverse('members.view', args=(lending.member.pk, 0,)))
-    return render(request, return_book_template, {'hx_fallback': not hx_enabled,'item': item,
+    return render(request, return_book_template, {'hx_enabled':  hx_enabled,'item': item,
                                                   'lending': lending,
                                                   'late': lending.end_date < get_today(),
                                                   'days_late': late_days.days,
