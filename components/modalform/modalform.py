526a1e040367ed6ba11138afc94d7e04c99536e9
Nander Voortman <nander@nander.net>
2025-01-24 15:48:13 +0100

    Fix linting.

--- components/modalform/modalform.py
+++ components/modalform/modalform.py
@@ -5,7 +5,7 @@ from django_components import Component, register
 class ModalForm(Component):
     template_name = "modalform/modalform.html"
 
-    def get_context_data(self, path="#", title= "", button_name="Submit", button_type="primary", btn_action="submit"):
+    def get_context_data(self, path="#", title="", button_name="Submit", button_type="primary", btn_action="submit"):
         return {
             "form_path": path,
             "form_title": title,
