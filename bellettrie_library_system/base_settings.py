fde22fc945598e30368d1c36cd227679d78c51bf
Nander Voortman <nanderv@gmail.com>
2025-02-17 22:47:25 +0100

    Some code cleanup; switch to a more standard cancel modal.

--- bellettrie_library_system/base_settings.py
+++ bellettrie_library_system/base_settings.py
@@ -86,7 +86,6 @@ TEMPLATES = [
                 'tables.templatetags.render_square',
 
                 'tables.templatetags.publications_list_for_member',
-                'tables.templatetags.reservation_table',
                 'tables.templatetags.item_detail_table',
                 'django_components.templatetags.component_tags',
             ],
