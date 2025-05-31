ce0fdf18fc2993c6eeee619240927ce909634080
Nander Voortman <nanderv@gmail.com>
2025-05-31 10:42:24 +0200

    Fix scaling, add datarow object, use datarow thingy for showing publication details.

---
+++ components/datarow/datarow.py
@@ -0,0 +1,14 @@
+from django_components import Component, register
+
+
+
+@register("datarow/datarow")
+class BaseTile(Component):
+    def get_context_data(self, name, root_class_tags=""):
+        return {
+            "name": name,
+            "root_class_tags": root_class_tags,
+        }
+
+    template_name = "datarow/datarow.html"
+
