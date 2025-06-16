088b5a2362a8bbe84848290cd74b9f2060ed4819
Nander Voortman <nanderv@gmail.com>
2025-06-16 20:39:23 +0200

    Fix code.

--- components/help_bar.py
+++ components/help_bar.py
@@ -1,13 +1,4 @@
-from typing import Optional
-
-from django.template.context import Context
-
-from django.urls import reverse
-from django_components import Component, register, types
-
-from bellettrie_library_system import settings
-from bellettrie_library_system.base_settings import GET_MENU
-
+from django_components import Component, register
 
 @register("help_bar")
 class HelpBar(Component):
