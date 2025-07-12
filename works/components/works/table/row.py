33186e6a8b0feceba3b120dada2f9ff9fe4ef4b3
Nander Voortman <nanderv@gmail.com>
2025-07-12 10:12:49 +0200

    Some layouting tests.

--- works/components/works/table/row.py
+++ works/components/works/table/row.py
@@ -12,15 +12,19 @@ class Row(Component):
     # Renders a table row for a single item.
     # all_authors is used to tell the table to show more than one author for each item (if more are linked)
     # skip_header is used to tell the row to not render the item code column.
-    def get_context_data(self, item: Item, all_authors=False, skip_header=False):
+    def get_context_data(self, item: Item, all_authors=False, skip_header=False, work=None):
         code = item.book_code
-        authors = item.work.get_deduplicated_authors()
+        work = work or item.work
+        authors = work.get_deduplicated_authors()
         if not all_authors and len(authors) > 0:
             authors = authors[:1]
+        print(work, work.title, item.work.title)
         return {
             "skip_header": skip_header,
             "authors": authors,
             "item": item,
+            "work": work,
+            "item_work": item.work,
             "split_code": code.split("-"),
         }
 
--- works/components/works/table/standard_card/card.py
+++ works/components/works/table/standard_card/card.py
@@ -5,13 +5,14 @@ from works.models import Item
 
 @register("works.table.standard_card.Card")
 class Card(Component):
-    def get_context_data(self, item: Item, perms) -> dict:
+    def get_context_data(self, item: Item, perms, work=None) -> dict:
         code = item.book_code
         authors = item.work.get_deduplicated_authors()
 
         authors = authors[:1]
         return {
             "item": item,
+            "work": work or item.work,
             "authors": authors,
             "split_code": code.split("-"),
         }
