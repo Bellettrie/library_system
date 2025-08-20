18cb2446f3464a71babed55caa6479a81a743760
Nander ... <nander@bellettrie.utwente.nl>
2025-08-20 23:46:44 +0200

    Update urls.py

--- bellettrie_library_system/urls.py
+++ bellettrie_library_system/urls.py
@@ -59,5 +59,5 @@ urlpatterns = ([
                    path('healthz/', ok),
                    path('readyz/', ok),
                    path('livez/', ok),
-               ]
-               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+               ])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
