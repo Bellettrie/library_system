ccb417a9e4f4a00c63ec047d5bbc8966584ff1b6
Nander Voortman <nanderv@gmail.com>
2025-09-21 16:45:16 +0200

    Add extra types to the admin interface.

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from works.models import ItemType, ItemState, Category, Location
+
 # Register your models here.
+
+admin.site.register(ItemType)
+admin.site.register(ItemState)
+
+admin.site.register(Category)
+admin.site.register(Location)
+
