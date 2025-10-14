a1b11ef86d81de7a7cbc8546977e19127ed57bf9
Nander Voortman <nanderv@gmail.com>
2025-10-14 18:33:14 +0200

    fix creator_books list.

--- creators/procedures/creator_books.py
+++ creators/procedures/creator_books.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from creators.models import Creator
 from series.models import Series
 from works.models import Publication
+from works.views import query_annotate_and_sort_bookcodes
 
 
 def get_books_for_author(creator: Creator):
