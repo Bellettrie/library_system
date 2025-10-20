ea0f4721ecef3974cb7f058298f851c283c47845
Nander Voortman <nanderv@gmail.com>
2025-10-20 11:06:15 +0200

    Add works and publications to admin panel.

--- works/admin.py
+++ works/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from works.models import ItemType, ItemState, Category, Location
+from works.models import ItemType, ItemState, Category, Location, Work, SubWork, Publication
 
 # Register your models here.
 
@@ -9,3 +9,13 @@ admin.site.register(ItemState)
 
 admin.site.register(Category)
 admin.site.register(Location)
+
+class AdminDta(admin.ModelAdmin):
+    list_display=('id', 'title', 'listed_author')
+    search_fields = ['id', 'title', 'sub_title', 'original_title', 'original_subtitle']
+    search_help_text = "Super simple search"
+admin.site.register(Work, AdminDta)
+
+admin.site.register(SubWork, AdminDta)
+
+admin.site.register(Publication, AdminDta)
