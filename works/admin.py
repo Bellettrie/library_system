c66451390e0ca1ae84f296e395aab3a1fc459bf5
Nander ... <nander@bellettrie.utwente.nl>
2025-10-20 11:41:29 +0200

    Fix linting.

--- works/admin.py
+++ works/admin.py
@@ -10,12 +10,13 @@ admin.site.register(ItemState)
 admin.site.register(Category)
 admin.site.register(Location)
 
+
 class AdminDta(admin.ModelAdmin):
-    list_display=('id', 'title', 'listed_author')
+    list_display = ('id', 'title', 'listed_author')
     search_fields = ['id', 'title', 'sub_title', 'original_title', 'original_subtitle']
     search_help_text = "Super simple search"
+
+
 admin.site.register(Work, AdminDta)
-
 admin.site.register(SubWork, AdminDta)
-
 admin.site.register(Publication, AdminDta)
