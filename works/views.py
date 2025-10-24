67d71cd83dc197763b1514d09fdd03e95d1ce7af
Nander ... <nander@bellettrie.utwente.nl>
2025-10-24 21:42:39 +0200

    Refine query ordering and distinct fields

--- works/views.py
+++ works/views.py
@@ -81,8 +81,8 @@ def query_annotate_and_sort_bookcodes(query):
         book_code=F('item__book_code'),
         book_code_extension=F('item__book_code_extension')
     )
-    query = query.order_by("book_code_sortable")
-    query = query.distinct("itemid", "id")
+    query = query.order_by("book_code_sortable", "id")
+    query = query.distinct("book_code_sortable", "id")
     return query
 
 
