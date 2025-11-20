d7facfbeb425253616a3be20d747166ba58a752b
Nander Voortman <nanderv@gmail.com>
2025-11-20 09:24:38 +0100

    Fix creatorbooks.

--- creators/procedures/creator_books.py
+++ creators/procedures/creator_books.py
@@ -1,8 +1,8 @@
 from django.db.models import Q
 
 from creators.models import Creator
-from series.models import Series
-from works.models import Work
+from series.models import Series, WorkInSeries
+from works.models import Work, CreatorToWork, WorkRelation
 from works.views import query_annotate_and_sort_bookcodes
 
 
@@ -13,13 +13,19 @@ def get_books_for_author(creator: Creator):
         series_len = len(series)
         series = series | set(Series.objects.filter(part_of_series__in=series))
 
-    query = (
-        Work.objects.filter(
-            Q(creatortowork__creator=creator)
-            | Q(workinseries__part_of_series__in=series)
-            | Q(work_in_publication_root__work__creatortowork__creator=creator)
-        )
-    )
+    work_series = WorkInSeries.objects.filter(part_of_series__in=series)
 
+    start_works = CreatorToWork.objects.filter(creator=creator)
+    work_ids = []
+    for work in start_works:
+        work_ids.append(work.work.id)
+
+    link_works = WorkRelation.RelationTraversal.for_search_words_inverse(work_ids)
+    for link_work in link_works:
+        work_ids.append(link_work.from_work.id)
+        work_ids.append(link_work.to_work.id)
+    for ll in work_series:
+        work_ids.append(ll.work.id)
+    query = Work.objects.filter(id__in=work_ids)
     q = query_annotate_and_sort_bookcodes(query)
     return q.all()
