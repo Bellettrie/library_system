df165035ef1ef5ef4980ac45526f06ebf0be6b77
Nander Voortman <nanderv@gmail.com>
2025-11-28 14:13:13 +0100

    Fix row data filling.

--- works/templatetags/publication_list_to_item_rows.py
+++ works/templatetags/publication_list_to_item_rows.py
@@ -7,13 +7,21 @@ from works.models.row_data import RowData
 
 @register.simple_tag
 def get_item_rows_for_publications(publications: List[Work]) -> List[RowData]:
-    rows = []
+    item_ids_for_publications = set()
     for publication in publications:
-        if hasattr(publication, 'itemid') and publication.itemid and hasattr(publication, 'book_code') and hasattr(
-                publication, 'book_code_extension'):
-            item = Item(publication=publication, book_code=publication.book_code,
-                        book_code_extension=publication.book_code_extension, id=publication.itemid)
-            rows.append(RowData(item=item, work=publication))
-        else:
-            rows.append(RowData(work=publication))
-    return rows
+        if hasattr(publication, 'itemid') and publication.itemid:
+            item_ids_for_publications.add(publication.itemid)
+
+    items = Item.objects.filter(id__in=item_ids_for_publications)
+    item_map = {}
+    for item in items:
+        item_map[item.id] = item
+
+    result = []
+    for publication in publications:
+        row_item = None
+        if hasattr(publication, 'itemid'):
+            row_item = item_map.get(publication.itemid, None)
+        result.append(RowData(item=row_item, work=publication))
+
+    return result
