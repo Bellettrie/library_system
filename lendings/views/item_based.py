c550bbadbe16f8d0d8af91b6c091c0efa385ca03
Remco Swenker <rswenker@gmail.com>
2025-12-05 14:48:13 +0100

    Bug fix now putting a rowdata element in the Card element.

--- lendings/views/item_based.py
+++ lendings/views/item_based.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 
 from works.models import Item
+from works.models.row_data import RowData
 
 
 @permission_required('lendings.add_lending')
@@ -10,5 +11,7 @@ def item_based(request, work_id):
     from utils.get_query_words import get_query_words
     words = get_query_words(request.GET.get("q"))
     members = query_members(words)
+    item = get_object_or_404(Item, pk=work_id)
+    rowdata = RowData(item=item, work=item.publication)
     return render(request, 'lendings/based_on_work.html',
-                  {'members': members, 'item': get_object_or_404(Item, pk=work_id)})
+                  {'members': members, 'item': item, 'rowdata': rowdata})
